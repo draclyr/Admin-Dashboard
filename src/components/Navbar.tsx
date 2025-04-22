6cf53b2ec172f85f8b4fc2686c0045f99262cb6f
Draclyr ( Hesam Shahmoradi ) <draclyr@gmail.com>
2025-04-22 13:39:52 -0700

    navbar v1.1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../../public/logo.png';
+import { Avatar, AvatarImage, AvatarFallback } from '@radix-ui/react-avatar';
 
 const Navbar = () => {
     return (
         <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between'>
             <Link href='#'>
                 <Image src={logo} alt='Admin-Dashboard' width={50} />
+                <Avatar>
+                    <AvatarImage />
+                </Avatar>
             </Link>
         </div>
     )
