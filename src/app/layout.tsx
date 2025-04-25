08c76751973b613b6454c661100d8e0f471921ab
Draclyr ( Hesam Shahmoradi ) <draclyr@gmail.com>
2025-04-25 14:01:13 -0700

    fix sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <Navbar />
         <div className="flex">
-          <div className='hidden md:block h-[100vh]'>
+          <div className='hidden md:block h-[100vh] w-[300px]'>
             <Sidebar />
           </div>
           <div className='p-5 w-full md:max-2-[1140px]'>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,20 +14,41 @@ import Link from "next/link";
 
 const Sidebar = () => {
     return (
-        <Command>
+        <Command className='bg-secondary'>
             <CommandInput placeholder="Type a command or search..." />
             <CommandList>
                 <CommandEmpty>No results found.</CommandEmpty>
                 <CommandGroup heading="Suggestions">
-                    <CommandItem>Calendar</CommandItem>
-                    <CommandItem>Search Emoji</CommandItem>
-                    <CommandItem>Calculator</CommandItem>
+                    <CommandItem>
+                        <LayoutDashboard className='mr-2 h-4 w-4' />
+                        <Link href='#'>Dashboard</Link>
+                    </CommandItem>
+                    <CommandItem>
+                        <Newspaper className='mr-2 h-4 w-4' />
+                        <Link href='/posts'>Posts</Link>
+                    </CommandItem>
+                    <CommandItem>
+                        <Folders className='mr-2 h-4 w-4' />
+                        <Link href='#'>Categories</Link>
+                    </CommandItem>
                 </CommandGroup>
                 <CommandSeparator />
                 <CommandGroup heading="Settings">
-                    <CommandItem>Profile</CommandItem>
-                    <CommandItem>Billing</CommandItem>
-                    <CommandItem>Settings</CommandItem>
+                    <CommandItem>
+                        <User className='mr-2 h-4 w-4' />
+                        <span>Profile</span>
+                        <CommandShortcut>P</CommandShortcut>
+                    </CommandItem>
+                    <CommandItem>
+                        <CreditCard className='mr-2 h-4 w-4' />
+                        <span>Billing</span>
+                        <CommandShortcut>B</CommandShortcut>
+                    </CommandItem>
+                    <CommandItem>
+                        <Settings className='mr-2 h-4 w-4' />
+                        <span>Settings</span>
+                        <CommandShortcut>S</CommandShortcut>
+                    </CommandItem>
                 </CommandGroup>
             </CommandList>
         </Command>
