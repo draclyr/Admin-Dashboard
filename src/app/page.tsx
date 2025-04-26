173ea48713be9b61cb4cde17f8e9f2ad4a26163c
Draclyr ( Hesam Shahmoradi ) <draclyr@gmail.com>
2025-04-26 14:08:12 -0700

    update dashboardcard v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-        <DashboardCard />
+        <DashboardCard
+          title='Posts'
+          count={90}
+          icon={<Newspaper className='text-slate-500' size={72} />}
+        />
+        <DashboardCard
+          title='Categories'
+          count={16}
+          icon={<Folder className='text-slate-500' size={72} />}
+        />
+        <DashboardCard
+          title='Users'
+          count={900}
+          icon={<User className='text-slate-500' size={72} />}
+        />
+        <DashboardCard
+          title='Comments'
+          count={642}
+          icon={<MessageCircle className='text-slate-500' size={72} />}
+        />
       </div>
     </>
   );
