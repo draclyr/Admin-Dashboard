bf34aa8beb6aaecaa870c6e21f6542f97fc5be67
Draclyr ( Hesam Shahmoradi ) <draclyr@gmail.com>
2025-04-26 13:39:37 -0700

    update dashboardcard v1

--- src/components/dashboard/DashboardCard.tsx
+++ src/components/dashboard/DashboardCard.tsx
@@ -1,6 +1,13 @@
+import { Card, CardContent } from "../ui/card";
+import { Newspaper } from "lucide-react";
+
 const DashboardCard = () => {
     return (
-        <div className="">Card</div>
+        <Card className="bg-slate-100 dark:bg-slate-800 p-4 pb-0">
+            <CardContent>
+                <h3 className="text-3x1 text-center mb-4 font-bold text-slate-500 dark:text-slate-200">Posts</h3>
+            </CardContent>
+        </Card>
     );
 }
 
